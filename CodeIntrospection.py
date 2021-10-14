5b03123df9f3b16a9c7ed3c5db41c2c7c171904b
fariha <fariha_ahmed27@hotmail.com>
2021-10-14 18:53:47 +0500

    14-10-2021

---
+++ CodeIntrospection.py
@@ -0,0 +1,26 @@
+#help function
+#print(help()) #show information
+#print(help("modules")) #show Modules
+#print(help("modules keyword")) #show keyword module
+
+#keyword 
+import keyword
+#print(keyword.kwlist)
+#print(keyword.__all__)
+
+#sys module
+import sys
+#print(sys.executable)
+#print(sys.platform)
+#print(sys.version)
+#print(sys.path)
+import tkinter
+#dir Function
+#print(dir(keyword))
+#print(dir(tkinter))
+#print(dir(sys))
+
+#print(dir())
+#print(dir(__builtins__))
+print(dir('sample'))
+
